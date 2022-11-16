53c99da2b44deee696d6fd12234a456ef0423926
Midyadi <midyadi20041106@gmail.com>
2022-11-16 20:45:07 +0300

    typo ticks

--- solar_model.py
+++ solar_model.py
@@ -11,7 +11,7 @@ def calculate_force(body, space_objects):
     Вычисляет силу, действующую на тело.
 
     Args:
-        body — тело, для которого нужно вычислить дейстующую силу
+        body — тело, для которого нужно вычислить действующую силу
 
         space_objects — список объектов, которые воздействуют на тело
     """
@@ -20,7 +20,7 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body != obj:
             r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
-            r = max(r, body.R)  # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+            r = max(r, body.R, obj.R)
             f = gravitational_constant * obj.m * body.m / r ** 2
             alpha = math.atan2((body.y - obj.y), (body.x - obj.x))
             body.Fx -= f * math.cos(alpha)
